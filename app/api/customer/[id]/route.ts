4bad9d7e1c76681fae4dfba60d2230f088cdbd30
Tanujeet <singhtanujeet@gmail.com>
2025-09-26 08:34:45 +0530

    Added:DELETE Api logic in customer/[id]/route.ts

--- app/api/customer/[id]/route.ts
+++ app/api/customer/[id]/route.ts
@@ -40,3 +40,50 @@ export async function GET(
     return new NextResponse("Failed to fetch customer", { status: 500 });
   }
 }
+
+
+
+
+
+
+export async function DELETE(
+  req: Request,
+  { params: paramsPromise }: { params: Promise<{ id: string }> }
+) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("unauthorized", { status: 403 });
+  }
+  const { id } = await paramsPromise;
+  try {
+    const customer = await prisma.customer.findUnique({ where: { id } });
+    if (!customer || customer.isDeleted) {
+      return new NextResponse("Customer not found", { status: 404 });
+    }
+
+    const membership = await prisma.membership.findMany({
+      where: { userId },
+      select: { teamId: true },
+    });
+
+    const teamIds = membership.map((m) => m.teamId);
+
+    if (!teamIds.includes(customer.teamId)) {
+      return new NextResponse("Forbidden: Not part of this team", {
+        status: 403,
+      });
+    }
+    const updatedCustomer = await prisma.customer.update({
+      where: { id },
+      data: { isDeleted: true },
+    });
+
+    return NextResponse.json({
+      message: "Customer deleted successfully",
+      customer: updatedCustomer,
+    });
+  } catch (err) {
+    console.error("Failed to delete customer", err);
+    return new NextResponse("Failed to delete customer", { status: 500 });
+  }
+}
