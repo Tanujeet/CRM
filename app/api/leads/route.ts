e0c3da9c6d382c23936e431144f3ed69c7b50e92
Tanujeet <singhtanujeet@gmail.com>
2025-09-27 08:07:58 +0530

    Added:GET and POST boilerplate Api logic in leads/route.ts

--- app/api/leads/route.ts
+++ app/api/leads/route.ts
@@ -1,3 +1,25 @@
-export async function GET(req:Request) {
-    
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+  try {
+  } catch (err) {
+    console.error("Failed to fetch leads", err);
+    return new NextResponse("Failed to fetch leads", { status: 500 });
+  }
+}
+export async function POST(req: Request) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+  try {
+  } catch (err) {
+    console.error("Failed to update leads", err);
+    return new NextResponse("Failed to update leads", { status: 500 });
+  }
 }
