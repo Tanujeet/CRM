6d998bb3cce8ea99254510f3faeb9dd931910432
Tanujeet <singhtanujeet@gmail.com>
2025-09-29 08:08:24 +0530

    Added:POST API logic inTasks/route.ts

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -13,24 +13,37 @@ export async function POST(req: Request) {
   if (!title || !teamId) {
     return new NextResponse("Title and TeamId is missing", { status: 404 });
   }
-  try {
-    const memberships = await prisma.membership.findMany({
-      where: { userId },
-      select: { teamId: true },
-    });
-    const teamIds = memberships.map((m) => m.teamId);
-    if (!teamIds.includes(teamId)) {
-      return new NextResponse("Forbidden: Not part of this team", {
-        status: 403,
-      });
-    }
 
-    const createTask = await prisma.task.create({
-      data: { title, description, dueDate, leadId, assignedTo, teamId },
-    });
-    return NextResponse.json(createTask);
-  } catch (err) {
-    console.error("failed to create tasks", err);
-    return new NextResponse("Failed to create task", { status: 500 });
-  }
+    try {
+      if (assignedTo) {
+        const assignedMembership = await prisma.membership.findFirst({
+          where: { userId: assignedTo, teamId },
+        });
+
+        if (!assignedMembership) {
+          return new NextResponse("Forbidden: Assignee not part of this team", {
+            status: 403,
+          });
+        }
+      }
+
+      const memberships = await prisma.membership.findMany({
+        where: { userId },
+        select: { teamId: true },
+      });
+      const teamIds = memberships.map((m) => m.teamId);
+      if (!teamIds.includes(teamId)) {
+        return new NextResponse("Forbidden: Not part of this team", {
+          status: 403,
+        });
+      }
+
+      const createTask = await prisma.task.create({
+        data: { title, description, dueDate, leadId, assignedTo, teamId },
+      });
+      return NextResponse.json(createTask);
+    } catch (err) {
+      console.error("failed to create tasks", err);
+      return new NextResponse("Failed to create task", { status: 500 });
+    }
 }
