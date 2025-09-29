9143feb895730c928fb931cc15369dc53f99ca95
Tanujeet <singhtanujeet@gmail.com>
2025-09-29 07:46:35 +0530

    Added: PATCH API logic in  leads/[id]route.ts

--- app/api/leads/[id]/route.ts
+++ app/api/leads/[id]/route.ts
@@ -44,12 +44,35 @@ export async function PATCH(
   if (!userId) {
     return new NextResponse("Unauthorized", { status: 403 });
   }
-  const { id } = await paramsPromise;
-  try {
-  } catch (err) {
-    console.error("Failed to get one lead", err);
-    return new NextResponse("Failed to get one lead", { status: 500 });
-  }
+    const { id } = await paramsPromise;
+    const { status, value, assignedTo } = await req.json();
+    try {
+      const lead = await prisma.lead.findUnique({ where: { id } });
+
+      if (!lead || lead.isDeleted) {
+        return new NextResponse("Lead not found", { status: 404 });
+      }
+
+      const memberships = await prisma.membership.findMany({
+        where: { userId },
+        select: { teamId: true },
+      });
+      const teamIds = memberships.map((m) => m.teamId);
+      if (!teamIds.includes(lead.teamId)) {
+        return new NextResponse("Forbidden: Not part of this team", {
+          status: 403,
+        });
+      }
+
+      const updateLead = await prisma.lead.update({
+        where: { id },
+        data: { status, value, assignedTo },
+      });
+      return NextResponse.json(updateLead);
+    } catch (err) {
+      console.error("Failed to get one lead", err);
+      return new NextResponse("Failed to get one lead", { status: 500 });
+    }
 }
 
 export async function DELETE(
