56228a0bef6e13bfd19355c0500a8ea6e6e2218b
Tanujeet <singhtanujeet@gmail.com>
2025-09-29 08:03:34 +0530

    Added:POST API logic inTasks/route.ts

---
+++ app/api/tasks/route.ts
@@ -0,0 +1,36 @@
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+  const { title, description, dueDate, teamId, leadId, assignedTo } =
+    await req.json();
+
+  if (!title || !teamId) {
+    return new NextResponse("Title and TeamId is missing", { status: 404 });
+  }
+  try {
+    const memberships = await prisma.membership.findMany({
+      where: { userId },
+      select: { teamId: true },
+    });
+    const teamIds = memberships.map((m) => m.teamId);
+    if (!teamIds.includes(teamId)) {
+      return new NextResponse("Forbidden: Not part of this team", {
+        status: 403,
+      });
+    }
+
+    const createTask = await prisma.task.create({
+      data: { title, description, dueDate, leadId, assignedTo, teamId },
+    });
+    return NextResponse.json(createTask);
+  } catch (err) {
+    console.error("failed to create tasks", err);
+    return new NextResponse("Failed to create task", { status: 500 });
+  }
+}
