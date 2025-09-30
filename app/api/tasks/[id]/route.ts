d54b6973050e36a8d494648b948ddafa2b21c6c8
Tanujeet <singhtanujeet@gmail.com>
2025-09-30 07:52:16 +0530

    Added:GET API logic in Tasks/[id]route.ts

---
+++ app/api/tasks/[id]/route.ts
@@ -0,0 +1,76 @@
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params: paramsPromise }: { params: Promise<{ id: string }> }
+) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+  const { id } = await paramsPromise;
+
+  try {
+    const tasks = await prisma.task.findUnique({
+      where: { id },
+      include: { team: true, assignee: true, lead: true },
+    });
+    if (!tasks) {
+      return new NextResponse("Tasks not found", { status: 404 });
+    }
+    const memberships = await prisma.membership.findMany({
+      where: { userId },
+      select: { teamId: true },
+    });
+    const teamIds = memberships.map((m) => m.teamId);
+    if (!teamIds.includes(tasks.teamId)) {
+      return new NextResponse("Forbidden: Not part of this team", {
+        status: 403,
+      });
+    }
+
+    return NextResponse.json(tasks);
+  } catch (err) {
+    console.error("Failed to fetch one tasks", err);
+    return new NextResponse("Failed to fetch one tasks", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params: paramsPromise }: { params: Promise<{ id: string }> }
+) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+  const { id } = await paramsPromise;
+
+  try {
+  } catch (err) {
+    console.error("Failed to update tasks", err);
+    return new NextResponse("Failed to update tasks", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params: paramsPromise }: { params: Promise<{ id: string }> }
+) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+  const { id } = await paramsPromise;
+
+  try {
+  } catch (err) {
+    console.error("Failed to delete tasks", err);
+    return new NextResponse("Failed to delete one tasks", { status: 500 });
+  }
+}
