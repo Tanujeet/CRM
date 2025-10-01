25a916297269acc292cf15350973d0925748f5b1
Tanujeet <singhtanujeet@gmail.com>
2025-10-01 08:23:03 +0530

    Added:POST API in  comment/route.ts

--- app/api/comment/route.ts
+++ app/api/comment/route.ts
@@ -1,5 +1,22 @@
 import { auth } from "@clerk/nextjs/server";
+import { errorToJSON } from "next/dist/server/render";
 import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+  const { content, leadId, taskId } = await req.json();
+  if (!leadId || !taskId) {
+    return new NextResponse("leadId and taskid not found", { status: 404 });
+  }
+  try {
+  } catch (err) {
+    console.error("Failed to create comments", err);
+    return new NextResponse("Failed to create comment", { status: 500 });
+  }
+}
 
 export async function GET(req: Request) {
   const { userId } = await auth();
@@ -13,14 +30,3 @@ export async function GET(req: Request) {
   }
 }
 
-export async function POST(req: Request) {
-  const { userId } = await auth();
-  if (!userId) {
-    return new NextResponse("Unauthorized", { status: 403 });
-  }
-  try {
-  } catch (err) {
-    console.error("Failed to create comments", err);
-    return new NextResponse("Failed to create comment", { status: 500 });
-  }
-}
