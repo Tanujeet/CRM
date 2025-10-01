76e07c6083df26b08e75eab49b937bfd990a3191
Tanujeet <singhtanujeet@gmail.com>
2025-10-01 08:26:40 +0530

    Added:POST API in  comment/route.ts

--- app/api/comment/route.ts
+++ app/api/comment/route.ts
@@ -1,3 +1,4 @@
+import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { errorToJSON } from "next/dist/server/render";
 import { NextResponse } from "next/server";
@@ -8,10 +9,39 @@ export async function POST(req: Request) {
     return new NextResponse("Unauthorized", { status: 403 });
   }
   const { content, leadId, taskId } = await req.json();
-  if (!leadId || !taskId) {
+  if (!content) {
+    return new NextResponse("Content is required", { status: 400 });
+  }
+
+  if (!leadId && !taskId) {
     return new NextResponse("leadId and taskid not found", { status: 404 });
   }
   try {
+    if (leadId) {
+      const lead = await prisma.lead.findUnique({ where: { id: leadId } });
+      if (!lead) return new NextResponse("Lead not found", { status: 404 });
+    }
+
+    if (taskId) {
+      const task = await prisma.task.findUnique({ where: { id: taskId } });
+      if (!task) return new NextResponse("Task not found", { status: 404 });
+    }
+
+    const createComment = await prisma.comment.create({
+      data: {
+        content,
+        userId,
+        leadId: leadId || null,
+        taskId: taskId || null,
+      },
+      include: {
+        user: {
+          select: { id: true, name: true, image: true },
+        },
+      },
+    });
+
+    return NextResponse.json(createComment);
   } catch (err) {
     console.error("Failed to create comments", err);
     return new NextResponse("Failed to create comment", { status: 500 });
@@ -29,4 +59,3 @@ export async function GET(req: Request) {
     return new NextResponse("Failed to fetch comment", { status: 500 });
   }
 }
-
