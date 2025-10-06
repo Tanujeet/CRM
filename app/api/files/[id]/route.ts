ed1396d9c68fc11e6edd2745ccceabe5c874a7c9
Tanujeet <singhtanujeet@gmail.com>
2025-10-06 07:54:38 +0530

    Added:GET API Logic in  files/[id]route.ts folder

--- app/api/files/[id]/route.ts
+++ app/api/files/[id]/route.ts
@@ -1,19 +1,48 @@
+import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET(
+  req: Request,
+  { params: paramsPromise }: { params: Promise<{ id: string }> }
+) {
   const { userId } = await auth();
   if (!userId) {
     return new NextResponse("Unauthorized", { status: 403 });
   }
-
+  const { id } = await paramsPromise;
   try {
+    const file = await prisma.file.findUnique({
+      where: { id },
+      include: {
+        lead: true,
+        task: true,
+      },
+    });
+
+    if (!file) {
+      return new NextResponse("File not found", { status: 404 });
+    }
+    let teamId = null;
+    if (file.lead) teamId = file.lead.teamId;
+    if (file.task) teamId = file.task.teamId;
+
+    if (teamId) {
+      const membership = await prisma.membership.findFirst({
+        where: { userId, teamId },
+      });
+
+      if (!membership) {
+        return new NextResponse("Forbidden", { status: 403 });
+      }
+    }
+
+    return NextResponse.json(file);
   } catch (err) {
     console.error("Failed to fetch one file", err);
     return new NextResponse("failed to fetch one file", { status: 500 });
   }
 }
-
 
 export async function PATCH(req: Request) {
   const { userId } = await auth();
@@ -27,7 +56,6 @@ export async function PATCH(req: Request) {
     return new NextResponse("failed to update file", { status: 500 });
   }
 }
-
 
 export async function DELETE(req: Request) {
   const { userId } = await auth();
