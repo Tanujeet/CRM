fa2f23a36566f8edd236874cd6ef0b35235804ed
Tanujeet <singhtanujeet@gmail.com>
2025-10-06 08:04:13 +0530

    Added:DELETE API Logic in  files/[id]route.ts folder

--- app/api/files/[id]/route.ts
+++ app/api/files/[id]/route.ts
@@ -92,13 +92,44 @@ export async function PATCH(
   }
 }
 
-export async function DELETE(req: Request) {
+export async function DELETE(
+  req: Request,
+  { params: paramsPromise }: { params: Promise<{ id: string }> }
+) {
   const { userId } = await auth();
   if (!userId) {
     return new NextResponse("Unauthorized", { status: 403 });
   }
+  const { id } = await paramsPromise;
 
   try {
+    const file = await prisma.file.findUnique({
+      where: { id },
+      include: {
+        lead: true,
+        task: true,
+      },
+    });
+
+    if (!file) {
+      return new NextResponse("File not found", { status: 404 });
+    }
+    let teamId = null;
+    if (file.lead) teamId = file.lead.teamId;
+    if (file.task) teamId = file.task.teamId;
+
+    if (teamId) {
+      const membership = await prisma.membership.findFirst({
+        where: { userId, teamId },
+      });
+
+      if (!membership) {
+        return new NextResponse("Forbidden", { status: 403 });
+      }
+    }
+
+    const deleteFile = await prisma.file.delete({ where: { id } });
+    return NextResponse.json(deleteFile);
   } catch (err) {
     console.error("Failed to update file", err);
     return new NextResponse("failed to update file", { status: 500 });
