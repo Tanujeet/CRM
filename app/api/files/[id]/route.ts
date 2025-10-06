3e379867c80c08d13841cee8ce15cb574e6d9650
Tanujeet <singhtanujeet@gmail.com>
2025-10-06 07:58:03 +0530

    Added:PATCH API Logic in  files/[id]route.ts folder

--- app/api/files/[id]/route.ts
+++ app/api/files/[id]/route.ts
@@ -44,13 +44,48 @@ export async function GET(
   }
 }
 
-export async function PATCH(req: Request) {
+export async function PATCH(
+  req: Request,
+  { params: paramsPromise }: { params: Promise<{ id: string }> }
+) {
   const { userId } = await auth();
   if (!userId) {
     return new NextResponse("Unauthorized", { status: 403 });
   }
-
+  const { id } = await paramsPromise;
+  const { url } = await req.json();
   try {
+    const file = await prisma.file.findUnique({
+      where: { id },
+      include: {
+        lead: true,
+        task: true,
+      },
+    });
+
+    if (!file) {
+      return new NextResponse("File not found", { status: 404 });
+    }
+    let teamId = null;
+    if (file.lead) teamId = file.lead.teamId;
+    if (file.task) teamId = file.task.teamId;
+
+    if (teamId) {
+      const membership = await prisma.membership.findFirst({
+        where: { userId, teamId },
+      });
+
+      if (!membership) {
+        return new NextResponse("Forbidden", { status: 403 });
+      }
+    }
+
+    const updateFile = await prisma.file.update({
+      where: { id },
+      data: { url },
+    });
+
+    return NextResponse.json(updateFile);
   } catch (err) {
     console.error("Failed to update file", err);
     return new NextResponse("failed to update file", { status: 500 });
