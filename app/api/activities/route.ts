70cd5592ab900a7c4f2ed3268eb0d22647c1c8a5
Tanujeet <singhtanujeet@gmail.com>
2025-10-07 08:24:35 +0530

    Added:GET  API code in activities/route.ts folder

--- app/api/activities/route.ts
+++ app/api/activities/route.ts
@@ -1,19 +1,55 @@
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
-import { preloadStyle } from "next/dist/server/app-render/entry-base";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   const { userId } = await auth();
+
   if (!userId) {
     return new NextResponse("Unauthorized", { status: 403 });
   }
+
+  const { searchParams } = new URL(req.url);
+  const leadId = searchParams.get("leadId");
+
+  if (!leadId) {
+    return new NextResponse("leadId is required", { status: 400 });
+  }
+
   try {
+    const memberships = await prisma.membership.findMany({
+      where: { userId },
+      select: { teamId: true },
+    });
+    const teamIds = memberships.map((m) => m.teamId);
+
+    const lead = await prisma.lead.findUnique({
+      where: { id: leadId },
+      select: { teamId: true },
+    });
+
+    if (!lead) {
+      return new NextResponse("Lead not found", { status: 404 });
+    }
+
+    if (!teamIds.includes(lead.teamId)) {
+      return new NextResponse("Forbidden: Not part of this team", {
+        status: 403,
+      });
+    }
+
+    const fetchActivity = await prisma.activity.findMany({
+      where: { leadId, createdBy: userId },
+      orderBy: { createdAt: "desc" },
+    });
+
+    return NextResponse.json(fetchActivity);
   } catch (err) {
     console.error("Failed to fetch activities", err);
     return new NextResponse("Failed to fetch activities", { status: 500 });
   }
 }
+
 
 export async function POST(req: Request) {
   const { userId } = await auth();
