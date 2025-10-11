d4f22dd6c625518a20e80c72babe18ff9f458ddb
Tanujeet <singhtanujeet@gmail.com>
2025-10-11 08:15:44 +0530

    Added:Changes in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,27 +7,29 @@ const Navbar = () => {
   const { user, isLoaded } = useUser();
   return (
     <nav className="border border-b-black p-4">
-      <div className="relative w-[400px]">
-        <Search
-          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
-          size={18}
-        />
-        <input
-          type="text"
-          placeholder="Search"
-          className="border border-black rounded-2xl p-2 pl-10 w-full focus:outline-none"
-        />
-      </div>
-      <div className="flex items-center gap-3">
-        {user && (
-          <Image
-            src={user.imageUrl}
-            alt="User Avatar"
-            width={40}
-            height={40}
-            className="rounded-full border"
+      <div className="flex justify-between">
+        <div className="relative w-[400px]">
+          <Search
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+            size={18}
           />
-        )}
+          <input
+            type="text"
+            placeholder="Search"
+            className="border border-black rounded-2xl p-2 pl-10 w-full focus:outline-none"
+          />
+        </div>
+        <div className="flex items-center gap-3">
+          {user && (
+            <Image
+              src={user.imageUrl}
+              alt="User Avatar"
+              width={40}
+              height={40}
+              className="rounded-full border"
+            />
+          )}
+        </div>
       </div>
     </nav>
   );
