dce4ff7e67739d9e4327acb50bdd8adb63549b5f
Tanujeet <singhtanujeet@gmail.com>
2025-12-21 20:51:58 +0530

    Added : Minor Changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,16 @@
 import Navbar from "@/components/navbar";
 
 const Page = () => {
-  return <main></main>;
+  return (
+    <main>
+      <section>
+        <header>
+          <h1 className="text-">Hey,Tanu - Here your overview Today</h1>
+          <p>Friday,October 10,2025</p>
+        </header>
+      </section>
+    </main>
+  );
 };
 
 export default Page;
