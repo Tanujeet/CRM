0d8eb34da793a5c443a3f521010fe613c16a9c97
Tanujeet <singhtanujeet@gmail.com>
2025-12-30 21:28:37 +0530

    Added :Heading in Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,8 +5,12 @@ const Page = () => {
     <main>
       <section>
         <header>
-          <h1 className="text-">Hey,Tanu - Here your overview Today</h1>
-          <p>Friday,October 10,2025</p>
+          <div className="p-4">
+            <h1 className="text-4xl font-black ">
+              Hey,Harsh - Here your overview Today
+            </h1>
+            <p className="mt-4 font-light">Friday,October 10,2025</p>
+          </div>
         </header>
       </section>
     </main>
